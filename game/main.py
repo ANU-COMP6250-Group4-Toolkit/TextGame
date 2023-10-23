7e24a05ae0ecd1322e99b6aa1eaf96b17f9811fe
our0boros <863166709@qq.com>
2023-10-23 23:07:45 +1100

    [DEBUG] scenario

--- game/main.py
+++ game/main.py
@@ -1,8 +1,10 @@
 SCREEN_LENGTH = 90
+SCENARIO_DICT = {}
+DEBUG = True
 def read_plain_text(source):
     max_length = 0
     plain_text = []
-    f = open(source)
+    f = open(source, encoding='utf-8')
     line = f.readline()
     while line:
         if len(line) > max_length:
@@ -12,7 +14,6 @@ def read_plain_text(source):
     return plain_text, max_length
 
 def welcome_page():
-
     print(" ╭" + "─" * (SCREEN_LENGTH - 2) + "╮ ")
     welcome, max_length = read_plain_text("welcome.txt")
     for text in welcome:
@@ -24,7 +25,28 @@ def welcome_page():
     return input("Number: ")
 
 
+def init_scenario_file(source):
+    if DEBUG: print("===[ init_scenario_file ]===")
+    f = open(source, encoding='utf-8')
+    line = f.readline()
+    while line:
+        # get question id
+        try:
+            id = int(line.split(" // ")[0])
+            SCENARIO_DICT[id] = [line.split(" // ")[1]]
+        except:
+            SCENARIO_DICT[id].append(line.split(" // ")[1])
+    return
+
+
 def start_game():
+    if DEBUG: print("===[ start_game ]===")
+    init_scenario_file("scenario.txt")
+    question_id = 0
+    while question_id != -1:
+        for line in SCENARIO_DICT[question_id]:
+            print(line)
+        select = input("We choose: ")
     pass
 
 
