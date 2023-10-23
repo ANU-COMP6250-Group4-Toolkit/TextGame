68968340504002a9f366fa494c0706c3795850f4
our0boros <863166709@qq.com>
2023-10-23 23:19:31 +1100

    [UPDATE] basic structure

--- game/main.py
+++ game/main.py
@@ -1,5 +1,6 @@
 SCREEN_LENGTH = 90
 SCENARIO_DICT = {}
+OPTIONS_DICT = {}
 DEBUG = True
 def read_plain_text(source):
     max_length = 0
@@ -33,20 +34,50 @@ def init_scenario_file(source):
         # get question id
         try:
             id = int(line.split(" // ")[0])
-            SCENARIO_DICT[id] = [line.split(" // ")[1]]
+            SCENARIO_DICT[id] = [line.split(" // ")[1].replace("\n", "")]
         except:
-            SCENARIO_DICT[id].append(line.split(" // ")[1])
+            SCENARIO_DICT[id].append(line.split(" // ")[1].replace("\n", ""))
+        line = f.readline()
+    pass
+
+def init_option_jump_link(source):
+    if DEBUG: print("===[ init_option_jump_link ]===")
+    f = open(source, encoding='utf-8')
+    line = f.readline()
+    while line:
+        id_strings = line.split(" ")
+        id = int(id_strings[0])
+        # * case
+        if id_strings[1] == "*":
+            OPTIONS_DICT[(id, -1)] = int(id_strings[2])
+        else:
+            OPTIONS_DICT[(id, int(id_strings[1]))] = int(id_strings[2])
+        line = f.readline()
     return
+
+
+def end_game():
+    pass
 
 
 def start_game():
     if DEBUG: print("===[ start_game ]===")
     init_scenario_file("scenario.txt")
+    init_option_jump_link("options.txt")
+
     question_id = 0
     while question_id != -1:
         for line in SCENARIO_DICT[question_id]:
             print(line)
         select = input("We choose: ")
+        if (question_id, -1) in OPTIONS_DICT.keys():
+            question_id = OPTIONS_DICT[(question_id, -1)]
+        elif (question_id, select) in OPTIONS_DICT.keys():
+            question_id = OPTIONS_DICT[(question_id, select)]
+        else:
+            question_id = -1
+    
+    end_game()
     pass
 
 
