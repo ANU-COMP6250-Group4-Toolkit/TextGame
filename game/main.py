9d8f54e9faa2b71d2c1375ba4c7b36b16c77e6db
our0boros <863166709@qq.com>
2023-10-23 22:23:19 +1100

    [UPDATE] welcome page + 1

--- game/main.py
+++ game/main.py
@@ -7,19 +7,28 @@ def read_plain_text(source):
     while line:
         if len(line) > max_length:
             max_length = len(line)
-        plain_text.append(line)
+        plain_text.append(line.replace("\n", ""))
         line = f.readline()
     return plain_text, max_length
 
 def welcome_page():
 
-    print("=" * SCREEN_LENGTH)
+    print(" ╭" + "─" * (SCREEN_LENGTH - 2) + "╮ ")
     welcome, max_length = read_plain_text("welcome.txt")
     for text in welcome:
-        print(" " * ((SCREEN_LENGTH - max_length) // 2) + text, end="")
-    print("=" * SCREEN_LENGTH)
+        output = " │" + " " * (SCREEN_LENGTH - 4) + "┃ "
+        to_middle_space = (SCREEN_LENGTH - max_length - 4) // 2
+        output = output[: 2 + to_middle_space] + text + output[to_middle_space + len(text):]
+        print(output)
+    print(" ╰" + "━" * (SCREEN_LENGTH - 2) + "╯ ")
+    return input("Number: ")
+
+
+def start_game():
     pass
 
 
 if __name__ == "__main__":
-    welcome_page()
+    action = welcome_page()
+    if action == "1":
+        start_game()
