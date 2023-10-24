7a1a22f293e378b46726129e655d0c30f5718d5f
AvarbyKira <1248488996@qq.com>
2023-10-24 22:58:51 +1100

    add functions

--- game/Game.py
+++ game/Game.py
@@ -1,15 +1,4 @@
-class Story:
-    def __init__(self, story_code):
-        self.content = open("Stories/" + story_code + ".txt", "r", encoding='utf-8').read()
-
-    def tell(self):
-        # split the content into lines
-        lines = self.content.split("\n")
-        # print the lines one by one, when user press enter, print the next line
-        for line in lines:
-            print(line)
-            input()
-
+SCREEN_LENGTH = 90
 
 def getChoice():
     while True:
@@ -20,8 +9,27 @@ def getChoice():
             print("Invalid choice, please try again.")
 
 
+def read_plain_text(source):
+    max_length = 0
+    plain_text = []
+    f = open(source, encoding='utf-8')
+    line = f.readline()
+    while line:
+        if len(line) > max_length:
+            max_length = len(line)
+        plain_text.append(line.replace("\n", ""))
+        line = f.readline()
+    return plain_text, max_length
+
 def show_title():
-    pass
+    print(" ╭" + "─" * (SCREEN_LENGTH - 2) + "╮ ")
+    welcome, max_length = read_plain_text("welcome.txt")
+    for text in welcome:
+        output = " │" + " " * (SCREEN_LENGTH - 4) + "┃ "
+        to_middle_space = (SCREEN_LENGTH - max_length - 4) // 2
+        output = output[: 2 + to_middle_space] + text + output[to_middle_space + len(text):]
+        print(output)
+    print(" ╰" + "━" * (SCREEN_LENGTH - 2) + "╯ ")
 
 
 def calculate_ending(collaboration, skill, confidence):
@@ -53,7 +61,7 @@ if __name__ == '__main__':
     confidence = 0
 
     # intro
-    tell_story = Story("0")
+    tell_story("0")
 
     # start game
     show_title()
