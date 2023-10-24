6b59ec3ff9c349266adc61154d779b46777f4d58
AvarbyKira <1248488996@qq.com>
2023-10-24 22:41:52 +1100

    finished coding for main

--- game/Game.py
+++ game/Game.py
@@ -1,21 +1,72 @@
-isGameOver = False
+class Story:
+    def __init__(self, story_code):
+        self.content = open("Stories/" + story_code + ".txt", "r", encoding='utf-8').read()
+
+    def tell(self):
+        # split the content into lines
+        lines = self.content.split("\n")
+        # print the lines one by one, when user press enter, print the next line
+        for line in lines:
+            print(line)
+            input()
 
 
-def initGame():
-    isGameOver = False
+def getChoice():
+    while True:
+        choice = input("Your choice: ")
+        if choice == "1" or choice == "2" or choice == "3":
+            return choice
+        else:
+            print("Invalid choice, please try again.")
+
+
+def show_title():
+    pass
+
+
+def calculate_ending(collaboration, skill, confidence):
+    pass
+
+
+def show_endings():
+    pass
+
+
+def updateStatus(day, choice, collaboration, skill, confidence):
+    pass
+
+
+def tell_story(story_code):
+    story = open("Stories/" + story_code + ".txt", "r", encoding='utf-8').read()
+    # split the content into lines
+    lines = story.split("\n")
+    # print the lines one by one, when user press enter, print the next line
+    for line in lines:
+        print(line)
+        input()
 
 
 if __name__ == '__main__':
-    # init game
-    initGame()
-    # run game loop
-    while not isGameOver:
-        if not isCurrentStoryOver():
-            tellStory(currentStory)
-        else:
-            choice = currentStory.makeChoice()
-            currentStory = currentStory.getNextStory(choice)
+    # init
+    collaboration = 0
+    skill = 0
+    confidence = 0
 
-    # game over, print result
-    outcome = calculateOutcome()
-    tellStory(outcome)
+    # intro
+    tell_story = Story("0")
+
+    # start game
+    show_title()
+
+    # loop for five days
+    for i in range(5):
+        # day i
+        tell_story(str(i))
+        choice = getChoice()
+        collaboration, skill, confidence = updateStatus(str(i), choice, collaboration, skill, confidence)
+        tell_story(str(i) + choice)
+
+    # game over
+    ending_code = calculate_ending(collaboration, skill, confidence)
+    tell_story(ending_code)
+    show_endings()
